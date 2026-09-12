832f7c1a7fac800f1a3eaf35ab4bc466a86214dc
ivankunzins <ivankunzins@gmail.com>
2026-09-12 11:48:52 +0300

    Tune round economy and world configuration

--- src/ReplicatedStorage/Config.lua
+++ src/ReplicatedStorage/Config.lua
@@ -3,32 +3,36 @@ local Config = {}
 Config.Round = {
     Duration = 180,
     LifebuoyWindow = 20,
-    Intermission = 12,
+    Intermission = 10,
+    ResultsDuration = 7,
     MinimumPlayers = 1,
 }
 
 Config.Economy = {
-    StartingCoins = 0,
+    StartingCoins = 30,
     CoinValue = 1,
+    RareCoinValue = 5,
     LifebuoyCost = 25,
-    SurvivalReward = 35,
+    SurvivalReward = 40,
     ParticipationReward = 10,
 }
 
 Config.World = {
-    ArenaRadius = 115,
-    ArenaY = 8,
-    CoinCount = 45,
-    CoinRespawnSeconds = 3,
-    ObstacleCount = 18,
+    ArenaRadius = 105,
+    ArenaY = 12,
+    CoinCount = 54,
+    RareCoinCount = 6,
+    CoinRespawnSeconds = 4,
+    ObstacleCount = 24,
+    SpawnCount = 16,
 }
 
 Config.Flush = {
-    Duration = 9,
-    PullRadius = 125,
-    PullStrength = 115,
-    SpinStrength = 38,
-    SinkDepth = 30,
+    Duration = 10,
+    PullRadius = 115,
+    PullStrength = 125,
+    SpinStrength = 52,
+    SinkDepth = 34,
 }
 
 return Config
